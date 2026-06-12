b6c00037b362f84c7833c7b0f8bd76646a5acaf0
arnabmondal-eg <arnabmondal.reach@outlook.com>
2026-06-12 16:40:26 -0700

    Add Hex to Binary Helper

---
+++ backend/src/helper/binaryHelper/binaryHelper.c
@@ -0,0 +1,35 @@
+#include "binaryHelper.h"
+#include <stdio.h>
+
+uint8_t maskArr[] = {
+    BIT_7_MASK, BIT_6_MASK, BIT_5_MASK, BIT_4_MASK,
+    BIT_3_MASK, BIT_2_MASK, BIT_1_MASK, BIT_0_MASK
+};
+
+/**
+ * @brief Converts a passed 1 bit int array into binary array
+ * 
+ * @param inputHexArray Hex values to be converted
+ * @param length Length of inptu
+ * @param binary Array to write to
+ */
+void hexToBinary(uint8_t inputHexArray[], int length, char *binary) {
+    int cntr = 0;
+
+    // number of bits
+    for(int i = 1; i <= length; i++) {
+
+        // printf("%02X -> ", inputHexArray[i-1]);
+
+        // 8 bits in a byte
+        for (int j = 1; j <= 8; j++) {
+            binary[cntr] = (inputHexArray[i-1] & maskArr[j-1]) != 0 ? 1 : 0;
+
+            // printf("%d  ", binary[cntr]);
+
+            cntr++;
+        }
+
+        // printf("\n");
+    }
+}
---
+++ backend/src/helper/binaryHelper/binaryHelper.h
@@ -0,0 +1,18 @@
+#ifndef binaryHelper_h
+#define binaryHelper_h
+
+#include"../../header/header.h"
+#include <stdint.h>
+
+#define BIT_7_MASK 0x80
+#define BIT_6_MASK 0x40
+#define BIT_5_MASK 0x20
+#define BIT_4_MASK 0x10
+#define BIT_3_MASK 0x08
+#define BIT_2_MASK 0x04
+#define BIT_1_MASK 0x02
+#define BIT_0_MASK 0x01
+
+void hexToBinary(uint8_t[], int, char*);
+
+#endif
