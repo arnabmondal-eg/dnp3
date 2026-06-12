9401c113dd9ad7476f797032234b9ddcbb4979b8
arnabmondal-eg <arnabmondal.reach@outlook.com>
2026-06-12 16:40:11 -0700

    Add DLC extraction/analysis

--- backend/src/header/header.h
+++ backend/src/header/header.h
@@ -3,13 +3,21 @@
 
 #include <stdint.h>
 
-// dlc defs: primary to secondary function codes
+// dlc defs: primary to secondary function codes; PRM = 1
 // src: dnp3 spec v4 data link layer rev. 2007 02 03
-#define DLC_RESET_LINK_STATES 0     // reset of remote link
-#define DLC_TEST_LINK_STATES 2      // test function for link
-#define DLC_CONFIRMED_USER_DATA 3   // deliver application data, confirmation requested
-#define DLC_UNCONFIRMED_USER_DATA 4 // deliver application data, confirmation not requested
-#define DLC_REQUEST_LINK_STATUS 9   // request status of link
+#define DLC_RESET_LINK_STATES 0x00     // reset of remote link
+#define DLC_TEST_LINK_STATES 0x02      // test function for link
+#define DLC_CONFIRMED_USER_DATA 0x03   // deliver application data, confirmation requested
+#define DLC_UNCONFIRMED_USER_DATA 0x04 // deliver application data, confirmation not requested
+#define DLC_REQUEST_LINK_STATUS 0x09   // request status of link
+// secondary to primary; PRM = 0
+#define DLC_ACK 0x00       // positive acknoledgement (confirm type)
+#define DLC_NACK 0x01      // negative acknoledgement (confirm type)
+#define DLC_LINK_STATUS 0x0B   // status of link (respond type)
+#define DLC_NOT_SUPPORTED 0x0F
+
+// dlc mask
+#define DLC_FUNCTION_CODE_MASK 0x0F
 
 // total 10 bytes / 3 compiler blocks (2 unsed bytes)
 typedef struct dnp3h_sd {
@@ -26,7 +34,7 @@ typedef struct dnp3h_sd {
     uint16_t crc;  // cyclic redundancy check byte
 } dnp3h_st;
 
-extern dnp3h_st mkHeader(char []);
+extern dnp3h_st mkHeader(uint8_t []);
 // checks start bytes and crc byte to determine packet validity
 extern int checkHeaderValidity(dnp3h_st);
 
--- backend/src/main.c
+++ backend/src/main.c
@@ -3,20 +3,13 @@
 #include <stdint.h>
 #include "header/header.h"
 #include "helper/crcHelper/crc.h"
+#include "helper/binaryHelper/binaryHelper.h"
 
 int main() {
-    char test_header[] = {0x05, 0x64, 0x0D, 0xC4, 0x49, 0x03, 0x01, 0x00, 0x92, 0x9C};
+    uint8_t test_header[] = {0x05, 0x64, 0x0D, 0xC4, 0x49, 0x03, 0x01, 0x00, 0x92, 0x9C};
 
     dnp3h_st header = mkHeader(test_header);
-
-    if(checkHeaderValidity(header) == 1) {
-        printf("valid\n");
-    }
-    else {
-        printf("Invalid\n");
-    }
-
-    printHeader(header);
+    extractDLC(header);
 
     return 0;
 }
