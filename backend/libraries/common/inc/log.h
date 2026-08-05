517db66c6608ed22818882835a8c96f266b1a3fb
arnabmondal-eg <arnabmondal.reach@outlook.com>
2026-08-05 14:24:03 -0700

    Add logging library

---
+++ backend/libraries/common/inc/log.h
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <errno.h>
+#include <time.h>
+#include <string.h>
+
+#define LOG_ERR    "\033[31m"
+#define LOG_WARN "\033[33m"
+#define LOG_START "\033[32m"
+#define LOG_RESET  "\033[0m"
+
+#define INFO_CONSOLE 0
+#define INFO_FILE 1
+#define INFO_BOTH 2
+
+#ifndef log_h
+#define log_h
+
+void log_timestamp(FILE*);
+
+void log_program_start(const char*, FILE*);
+void log_program_terminate(const char*, FILE*);
+void log_program_crash(const char*, FILE*);
+
+void log_err(int, const char*, FILE*);
+void log_warn(int, const char*, FILE*);
+void log_info(const char*, FILE*, int);
+
+#endif
---
+++ backend/libraries/common/src/log.c
@@ -0,0 +1,116 @@
+#include "log.h"
+
+void log_timestamp(FILE *fptr) {
+    time_t currentTime;
+    struct tm *t;
+
+    time(&currentTime);
+    t = localtime(&currentTime);
+
+    if (fptr != NULL) {
+        fprintf(fptr,
+            "[%02d:%02d:%02d] ",
+
+            t -> tm_hour,
+            t -> tm_min,
+            t -> tm_sec
+        );
+    }
+}
+
+void log_program_start(const char *message, FILE *fptr) {
+    time_t currentTime;
+    time(&currentTime);
+
+    fprintf(stdout, LOG_START "\nStarted %s" LOG_RESET" at %s", message, ctime(&currentTime));
+    fprintf(fptr, "\nStarted %s at %s", message, ctime(&currentTime));
+
+    return;
+}
+
+void log_program_terminate(const char *message, FILE *fptr) {
+    time_t currentTime;
+    time(&currentTime);
+
+    fprintf(stdout, LOG_START "\nEnded %s" LOG_RESET" at %s", message, ctime(&currentTime));
+    fprintf(fptr, "\nEnded %s at %s", message, ctime(&currentTime));
+
+    return;
+}
+
+void log_program_crash(const char *message, FILE *fptr) {}
+
+
+/* TODO: Add ANSI Color escape codes */
+
+// Log Error to File and Console
+void log_err(int val_errno, const char *message, FILE *fptr) {
+
+    fprintf(stderr, LOG_ERR);   // paint the terminal in blood (color text red)
+    fprintf(stderr, "\n\t~~~~ FATAL ERROR DETECTED ~~~~\n");
+    log_timestamp(stderr);
+    fprintf(stderr, "[%s/ERROR]: %s\n" LOG_RESET, message, strerror(val_errno));
+
+    if (fptr != NULL) {
+        fprintf(fptr, "\n\t~~~~ FATAL ERROR DETECTED ~~~~\n");
+        log_timestamp(fptr);
+        fprintf(fptr, "%s: %s\n", message, strerror(val_errno));
+    }
+    
+    fflush(stderr);
+    if(fptr != NULL) fflush(fptr);
+
+    return;
+}
+
+/* Log Warrning to File and Console*/
+void log_warn(int val_errno, const char *message, FILE *fptr) {
+    log_timestamp(stderr);
+    fprintf(stderr, LOG_WARN "%s: %s\n" LOG_RESET, message, strerror(val_errno));    // console
+
+    if (fptr != NULL) {
+        log_timestamp(fptr);
+        fprintf(fptr, "%s: %s", message, strerror(val_errno));    // file
+    }
+
+    fflush(stderr);
+    if(fptr != NULL) fflush(fptr);
+    
+    return;
+}
+
+/* Log Info to File, Console, or Both*/
+void log_info(const char *message, FILE *fptr, int type) {
+    //TODO: Allow this to accepts args like printf
+
+    switch (type) {
+    case INFO_CONSOLE:
+
+        log_timestamp(stdout);
+        fprintf(stdout, "%s\n", message);
+
+        break;
+    case INFO_FILE:
+
+        if (fptr != NULL){
+            log_timestamp(fptr);
+            fprintf(fptr, "%s\n", message);
+        }
+
+        break;
+    
+    case INFO_BOTH:
+    default:
+    
+        log_timestamp(stdout);
+        fprintf(stdout, "%s\n", message);
+
+        if (fptr != NULL) {
+            log_timestamp(fptr);
+            fprintf(fptr, "%s\n", message);
+        }
+
+        break;
+    }
+    return;
+}
